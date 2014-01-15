b31895c69cc53eff60611c8b098761127dd3bff9
David Adalberth Andersen <davidadalberth@gmail.com>
2014-01-15 14:35:04 +0100

    Started on ex pages

--- gruntfile.js
+++ gruntfile.js
@@ -3,7 +3,8 @@ module.exports = function (grunt) {
     grunt.initConfig({
         watch: {
             files: ['_/sass/**/*.scss', '*.html','styleguide-template/**','gruntfile.js', 'config.rb'],
-            tasks: ['compass:dev','shell'],
+//            tasks: ['compass:dev', 'shell'],
+            tasks: ['compass:dev'],
             options: {
                 livereload: true,
             }
